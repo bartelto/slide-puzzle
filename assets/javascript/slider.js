cfa97d5741acc14094caf406d54fc0553b909060
Miles Standoffish <toddnan@gmail.com>
2019-07-09 11:45:15 -0500

    added ability to slide multiple pieces at a time

--- assets/javascript/slider.js
+++ assets/javascript/slider.js
@@ -1,4 +1,4 @@
-let piecesPerSide = 3;
+let piecesPerSide = 5;
 let puzzleWidth = 500;
 let pieceSize = Math.round(puzzleWidth/piecesPerSide);
 
@@ -33,15 +33,45 @@ if ($(document).ready()) {
 }
 
 $(".piece").on("click", function(event) {
-    //console.log("clicked: " + $(this).text());
-    let rowClicked = $(this).attr("data-row");
-    let colClicked = $(this).attr("data-col");
-    let rowEmpty = $(".empty").attr("data-row");
-    let colEmpty = $(".empty").attr("data-col");
+    
+    console.log("clicked: " + $(this).text());
+    let rowClicked = parseInt($(this).attr("data-row"));
+    let colClicked = parseInt($(this).attr("data-col"));
+    let rowEmpty = parseInt($(".empty").attr("data-row"));
+    let colEmpty = parseInt($(".empty").attr("data-col"));
 
-    if ((rowClicked === rowEmpty && Math.abs(colClicked - colEmpty) === 1) ||
-        (colClicked === colEmpty && Math.abs(rowClicked - rowEmpty) === 1)) {
+    if ((rowClicked === rowEmpty) || (colClicked === colEmpty)) {
         
+        // classify all pieces to be moved
+        let rowMax = rowClicked;
+        let rowMin = rowClicked;
+        let colMax = colClicked;
+        let colMin = colClicked;
+        if      (rowClicked > rowEmpty) rowMin = rowEmpty + 1;
+        else if (rowClicked < rowEmpty) rowMax = rowEmpty - 1;
+        else if (colClicked > colEmpty) colMin = colEmpty + 1;
+        else if (colClicked < colEmpty) colMax = colEmpty - 1;
+    
+        let increment = 0;
+        if (rowClicked > rowEmpty || colClicked > colEmpty) {
+            increment = 1;
+        } else {
+            increment = -1;
+        }
+        // data-move = 0 --> empty spot
+        
+        let moveIndex = 0; // will contain the number of pieces moving
+        for (let r = rowEmpty; r != rowClicked + increment; r+=increment) {
+            for (let c = colEmpty; c != colClicked + increment; c+=increment) {
+                //console.log(`row: ${r} col: ${c}`);
+                $(`.piece[data-row='${r}'][data-col='${c}']`)
+                    .addClass("moving")
+                    .attr("data-move", moveIndex);
+                    moveIndex++;
+            }
+        }
+
+        // set animation direction
         let move = "";
         if (rowClicked > rowEmpty) {
             move = {top: `-=${pieceSize}`};
@@ -53,21 +83,32 @@ $(".piece").on("click", function(event) {
             move = {left: `+=${pieceSize}`};
         }
 
-        $(this).animate( move, 300,
+        $(".moving").animate( move, 300,
             function () { 
-                $(this).css("top","").css("left", "");// reset the sliding animation
-                $(".empty").css("background-image", $(this).css("background-image"));
-                $(".empty").css("background-position", $(this).css("background-position"));
-                $(".empty").css("background-size", $(this).css("background-size"));
-                $(".empty").text($(this).text());
-                $(".empty").removeClass("empty");
-                $(this).text("");
-                $(this).addClass("empty");
-                $(this)
-                    .css("background-image","")
-                    .css("background-position","")
-                    .css("background-size","");
+                $(".piece").css("top","").css("left", "");// reset the sliding animation
+                // actually move piece data
 
+                for (let i = 0; i < moveIndex; i++) {
+                    let toSpot = $(`[data-move='${i}']`);
+                    let fromSpot = $(`[data-move='${i+1}']`);
+                    if (i < moveIndex-1) {
+                        toSpot.removeClass("empty");
+                        toSpot.css("background-image", fromSpot.css("background-image"));
+                        toSpot.css("background-position", fromSpot.css("background-position"));
+                        toSpot.css("background-size", fromSpot.css("background-size"));
+                        toSpot.text(fromSpot.text());
+                    } else { // new empty spot
+                        toSpot.text("");
+                        toSpot.addClass("empty");
+                        toSpot
+                            .css("background-image","")
+                            .css("background-position","")
+                            .css("background-size","");
+                    }
+                }
+                // remove move data
+                $(".piece").attr("data-move", "");
+                $(".piece").removeClass("moving");
             }
         );
         
